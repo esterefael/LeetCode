df1c118f4cb8f26a025c3cb17a5a16f3ea4e944b
Ety Refaelovich <ety0504102601@gmail.com>
2022-06-29 12:55:10 +0300

    Time: 9 ms (62.87%), Space: 47.8 MB (25.45%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,16 +8,29 @@
  * }
  */
 class Solution {
-    TreeNode commonAncestor = null;
+    // public TreeNode commonAncestor = null;
+    // public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    //     findLowestCommonAncestor(root, p, q);
+    //     return commonAncestor;
+    // }
+    // public boolean findLowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    //     if(root == null) return false;
+    //     boolean flag = root == p || root == q;
+    //     boolean left = findLowestCommonAncestor(root.left, p, q);
+    //     boolean right = findLowestCommonAncestor(root.right, p, q);
+    //     if(left && right || left && flag || right && flag) commonAncestor = root;
+    //     return left || right || flag;
+    // }
+    public TreeNode commonAncestor = null;
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         findLowestCommonAncestor(root, p, q);
         return commonAncestor;
     }
     public boolean findLowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if(root == null) return false;
-        boolean flag = root == p || root == q;
-        boolean left = findLowestCommonAncestor(root.left, p, q);
-        boolean right = findLowestCommonAncestor(root.right, p, q);
+        Boolean flag = root == p || root == q;
+        Boolean left = findLowestCommonAncestor(root.left, p, q);
+        Boolean right = findLowestCommonAncestor(root.right, p, q);
         if(left && right || left && flag || right && flag) commonAncestor = root;
         return left || right || flag;
     }
