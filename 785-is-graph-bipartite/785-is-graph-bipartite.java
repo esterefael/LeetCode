2e383b9407b521b4c2cec37ad1fa6e6d62bae0ed
Ety Refaelovich <ety0504102601@gmail.com>
2022-07-15 10:51:51 +0300

    Time: 1 ms (88.90%), Space: 53.3 MB (77.24%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,26 +1,54 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
-        Queue<Integer> queue = new LinkedList<>();
-        int[] group = new int[graph.length];
-        Arrays.fill(group, -1);
-        for(int i = 0; i < graph.length; i++){
-            if(group[i] == -1){
-                queue.offer(i);
-                group[i] = 1;
-                while(!queue.isEmpty()){
-                    int top = queue.poll();
-                    int curGroup = group[top];
-                    int nextGroup = 1 - curGroup;
-                    for(int neighbor:graph[top]){
-                        if(group[neighbor] == curGroup)return false;
-                        if(group[neighbor] == -1){
-                            queue.offer(neighbor);
-                            group[neighbor] = nextGroup;                    
-                        }
-                    }
-                }  
+    int[] color = new int[graph.length];
+    Arrays.fill(color, -1);
+    for (int i = 0; i < graph.length; i++) {
+        if (color[i] == -1) {
+            if (!bfsCheck(graph, i, color)) {
+                return false;
             }
         }
-        return true;
     }
+    return true;
+}
+ public  boolean bfsCheck(int[][] graph, int src, int[] color) {
+    Queue<Integer> que = new LinkedList<>();
+    que.add(src);
+    color[src] = 1;
+    while (!que.isEmpty()) {
+        int node = que.poll();
+        for (Integer it : graph[node]) {
+            if (color[it] == -1) {
+                color[it] = 1 - color[node];
+                que.add(it);
+            } else if (color[it] == color[node]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+//     public boolean isBipartite(int[][] graph) {
+//         Queue<Integer> queue = new LinkedList<>();
+//         int[] group = new int[graph.length];
+//         for(int i = 0; i < graph.length; i++){
+//             if(group[i] == 0){
+//                 queue.offer(i);
+//                 group[i] = 1;
+//                 while(!queue.isEmpty()){
+//                     int top = queue.poll();
+//                     int curGroup = group[top];
+//                     int nextGroup = curGroup ==1? 2: 1;
+//                     for(int neighbor:graph[top]){
+//                         if(group[neighbor] == curGroup)return false;
+//                         if(group[neighbor] == 0){
+//                             queue.offer(neighbor);
+//                             group[neighbor] = nextGroup;                    
+//                         }
+//                     }
+//                 }  
+//             }
+//         }
+//         return true;
+//     }
 }
