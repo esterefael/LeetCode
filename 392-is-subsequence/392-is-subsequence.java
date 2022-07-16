6200f3a44af06e4f7f7311552778771f8e154e72
Ety Refaelovich <ety0504102601@gmail.com>
2022-07-16 23:07:01 +0300

    Time: 5 ms (10.99%), Space: 44.3 MB (5.21%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.java
+++ 392-is-subsequence/392-is-subsequence.java
@@ -1,13 +1,36 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
         
-        int preIndex = -1, curIndex;
-        for(int i = 0; i < s.length(); i++){
-            curIndex = t.indexOf( s.charAt(i), preIndex + 1);
-            System.out.println(curIndex);
-            if(curIndex < 0) return false;
-            preIndex = curIndex;
+        // int preIndex = -1, curIndex;
+        // for(int i = 0; i < s.length(); i++){
+        //     curIndex = t.indexOf( s.charAt(i), preIndex + 1);
+        //     System.out.println(curIndex);
+        //     if(curIndex < 0) return false;
+        //     preIndex = curIndex;
+        // }
+        // return true;   
+        
+        // Follow up        
+        HashMap<Character, List<Integer>> hm = new HashMap<>();
+        for(int i = 0; i < t.length(); i++){
+            List<Integer> list = hm.getOrDefault(t.charAt(i), new LinkedList<>());
+            list.add(i);
+            hm.put(t.charAt(i), list);
         }
-        return true;        
+
+        int index = -1;
+        for(int i = 0; i < s.length(); i++){
+            boolean flag = false;
+            if(!hm.containsKey(s.charAt(i)))return false;
+            for(int j: hm.get(s.charAt(i))){
+                if(j > index){
+                    index = j;
+                    flag  = true;
+                    break;
+                }
+            }
+            if(!flag)return false;
+        }
+        return true;
     }
 }
