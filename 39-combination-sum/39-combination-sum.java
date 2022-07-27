871f317b5aec038a4f7642bae96d7aef17ed9f5f
Ety Refaelovich <ety0504102601@gmail.com>
2022-07-27 13:21:51 +0300

    Time: 19 ms (9.22%), Space: 51.2 MB (7.81%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> res= new LinkedList<>();
+        combinationSum(candidates, 0, target, new LinkedList<>(), res);
+        return res;
+    }
+    public void combinationSum(int[] candidates, int i, int target, List<Integer>cur, List<List<Integer>> res) {
+        
+        if(target <= 0){
+            
+            if(target == 0)
+                res.add(new LinkedList<>(cur));
+            return;
+        }
+        for(; i < candidates.length; i++){
+            
+            cur.add(candidates[i]);
+            combinationSum(candidates, i, target - candidates[i], cur, res);
+            cur.remove(cur.size() - 1);            
+        }
+    
+    }
+}
