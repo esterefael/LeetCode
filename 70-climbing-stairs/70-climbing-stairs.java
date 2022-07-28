cc92f06831b5c24b38dae5c55c3581f028277c8c
Ety Refaelovich <ety0504102601@gmail.com>
2022-07-28 10:15:35 +0300

    Time: 0 ms (100.00%), Space: 41.1 MB (33.22%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+        int prev1 = 1, prev2 = 1, cur;
+        
+        for(int i = 1; i < n; i++ ){
+            cur = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = cur;
+        }
+            
+        return prev1;
+    }
+}
