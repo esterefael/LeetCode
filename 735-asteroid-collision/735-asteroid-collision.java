3f59ae19a575fbe8718060b5e9570d6df0241952
Ety Refaelovich <ety0504102601@gmail.com>
2022-09-14 14:14:57 +0300

    Time: 15 ms (35.52%), Space: 48.5 MB (40.12%) - LeetHub

--- 735-asteroid-collision/735-asteroid-collision.java
+++ 735-asteroid-collision/735-asteroid-collision.java
@@ -1,32 +1,8 @@
-// class Solution {
-//     public int[] asteroidCollision(int[] asteroids) {
-//         Stack<Integer> s = new Stack<>();
-//         for(int a: asteroids){
-//             if(a > 0/* || !s.isEmpty() && s.peek() < 0*/){
-//                 s.push(a);
-//                 continue;
-//             }
-//             while(!s.isEmpty() && s.peek() > 0 && s.peek() < -a)
-//                 s.pop();
-//             if(!s.isEmpty() && s.peek() > -a)
-//                 continue;
-//             if(!s.isEmpty() && s.peek() == -a)
-//                 s.pop();
-//             else
-//                 s.push(a);                
-//         }
-//         int n = s.size();
-//         int[] res = new int[n];
-//         for(int i = n - 1; i >= 0; i--)
-//             res[i] = s.pop();
-//         return res;
-//     }
-// }
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         Stack<Integer> s = new Stack<>();
         for(int a: asteroids){
-            if(a > 0){
+            if(a > 0/* || !s.isEmpty() && s.peek() < 0*/){
                 s.push(a);
                 continue;
             }
